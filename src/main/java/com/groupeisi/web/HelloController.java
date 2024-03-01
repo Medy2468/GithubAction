809937ce8a7c221989f9cf49b1d0bdf91bd9629c
medy2468 <mnsangouisidk@groupeisi.com>
2024-03-01 15:16:09 +0000

    Github Action

---
+++ src/main/java/com/groupeisi/web/HelloController.java
@@ -0,0 +1,17 @@
+package com.groupeisi.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> welcome(){
+
+        return ResponseEntity.ok("Welcome to GithUb Action");
+    }
+}
